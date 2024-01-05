b1bf6d0f6e364842fab562fb938990ac9a13a234
gigaquantum <113395660+gigaquantum@users.noreply.github.com>
2024-01-04 19:57:57 -0500

    finished sorting algo

--- sortingAlgo.js
+++ sortingAlgo.js
@@ -1,34 +1,104 @@
-const groups = [
-  ["a", "b", "c", "d"],
-  ["e", "f", "g", "h"],
-  ["i", "j", "k", "l", "m", "n"],
-  ["o", "p", "q"],
-  ["r"],
-  ["s"],
-  ["t", "u"],
-  ["v", "w", "x"],
-  ["y", "z"],
+const testGroups = [
+  [
+    "Alice",
+    "Bob",
+    "Charlie",
+    "David",
+    "Ethan",
+    "Frank",
+    "George",
+    "Hannah",
+    "Iris",
+  ],
+  ["Jack", "Katie", "Liam", "Mia"],
+  ["Noah", "Olivia", "Peter", "Quinn", "Rachel", "Sam"],
+  ["Tom", "Uma", "Victor", "Wendy", "Xavier", "Yara", "Zane"],
+  ["Abby", "Ben", "Cindy", "Dan"],
+  ["Eva", "Fred", "Grace", "Harry", "Ivy"],
+  ["Jake", "Kelly", "Leo", "Molly", "Nate", "Oscar"],
+  ["Patty", "Quincy", "Rita", "Steve", "Tina", "Ursula", "Vince", "Walter"],
+  ["Xena", "Yoshi", "Zara", "Andy", "Bella", "Carl", "Daisy"],
 ];
 
-const tables = [
+/* const testTables = [
   { seats: 8, occupants: [] },
   { seats: 3, occupants: [] },
   { seats: 6, occupants: [] },
   { seats: 4, occupants: [] },
   { seats: 5, occupants: [] },
 ];
+ */
 
-// put the largest groups in the largest tables first
-groups.sort((a, b) => b.length - a.length);
-tables.sort((a, b) => b.seats - a.seats);
+const testTables = [4, 6, 8, 10, 7, 5, 8, 9, 7, 4];
 
-groups.forEach((group) => {
-  if (tables[0].seats >= group.length) {
-    tables[0].occupants.push(group);
-    tables[0].seats -= group.length;
-    tables.sort((a, b) => b.seats - a.seats);
-  } else
-    console.error("table configuration doesn't fit, moving to next algorithm");
-});
+const algoOptions = [
+  {
+    name: "Largest Groups --> Largest Tables (First)",
+    groupSort: function (a, b) {
+      return b.length - a.length;
+    },
+    tableSort: function (a, b) {
+      return b.unoccupiedSeats - a.unoccupiedSeats;
+    },
+  },
+  {
+    name: "Largest Groups --> Smallest Tables (First)",
+    groupSort: function (a, b) {
+      return b.length - a.length;
+    },
+    tableSort: function (a, b) {
+      return a.unoccupiedSeats - b.unoccupiedSeats;
+    },
+  },
+  {
+    name: "Smallest Groups --> Smallest Tables (First)",
+    groupSort: function (a, b) {
+      return a.length - b.length;
+    },
+    tableSort: function (a, b) {
+      return a.unoccupiedSeats - b.unoccupiedSeats;
+    },
+  },
+];
 
-console.log(tables);
+function sortTableSeats(groups, tables, groupSortFunc, tableSortFunc) {
+  tableObjs = [];
+  tables.forEach((table) => {
+    tableObjs.push({
+      capacity: table,
+      unoccupiedSeats: table,
+      occupants: [],
+    });
+  });
+  tableObjs.sort(tableSortFunc);
+  groups.sort(groupSortFunc);
+
+  for (let i = 0; i < groups.length; i++) {
+    if (tableObjs[0].unoccupiedSeats >= groups[i].length) {
+      tableObjs[0].occupants.push(groups[i]);
+      tableObjs[0].unoccupiedSeats -= groups[i].length;
+      tableObjs.sort(tableSortFunc);
+    } else {
+      console.log("table configuration doesn't fit, moving to next algorithm");
+      return null;
+    }
+  }
+  return tableObjs;
+}
+
+function mainSort(mainGroups, mainTables, mainAlgoOptions) {
+  let result = null;
+  for (let i = 0; i < algoOptions.length; i++) {
+    result = sortTableSeats(
+      mainGroups,
+      mainTables,
+      mainAlgoOptions[i].groupSort,
+      mainAlgoOptions[i].tableSort
+    );
+    if (result != null) break;
+  }
+  return result;
+}
+
+// test sorting function
+console.log(mainSort(testGroups, testTables, algoOptions));
