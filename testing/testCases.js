20a5f557c0e7c7078a9ecd4ad63f12c5df2e57ee
gigaquantum <113395660+gigaquantum@users.noreply.github.com>
2024-01-15 15:27:21 -0500

    changed test case logging
    changed test cases to log test status, and for failed cases only, test details

--- testing/testCases.js
+++ testing/testCases.js
@@ -1,9 +1,9 @@
 import { mainSort, rangeSort, algoFunctionOptions } from "./sortingAlgo.js";
 import { nameList } from "./testData.js";
 
-const testCases = 10000;
+const testCases = 100000;
 
-for (let loops = 0; loops < testCases; loops++) {
+for (let loops = 1; loops <= testCases; loops++) {
   let groupList = [];
   const groupQuantity = Math.round(Math.random() * 20 + 5);
   for (let i = 0; i < groupQuantity; i++) {
@@ -18,20 +18,29 @@ for (let loops = 0; loops < testCases; loops++) {
   const maxSeats = 15;
   const minSeats = Math.round(Math.random() * 6 + 4);
 
-  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+  /*   console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
   console.log(`
   Test Case: ${loops}
   Max Seats: ${maxSeats}
   Min Seats: ${minSeats}
   Groups (below):`);
   console.log(groupList);
-  console.log("---result---");
+  console.log("---result---"); */
 
-  console.log(rangeSort(groupList, algoFunctionOptions, maxSeats, minSeats));
-  /* try {
-    console.log(rangeSort(groupList, algoFunctionOptions, maxSeats, minSeats));
+  // console.log(rangeSort(groupList, algoFunctionOptions, maxSeats, minSeats));
+  try {
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    console.log(`Test Case: ${loops}`);
+    rangeSort(groupList, algoFunctionOptions, maxSeats, minSeats);
+    console.log(`Status: Passed`);
   } catch (error) {
+    console.log(`Status: Failed
+  Max Seats: ${maxSeats}
+  Min Seats: ${minSeats}
+  Groups (below):`);
+    console.log(groupList);
+    console.log("---error message---");
     console.error(error);
-    // alert(error);
-  } */
+    break;
+  }
 }
