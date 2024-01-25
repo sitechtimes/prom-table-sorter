759b9bcdad3c1d0e6b8d88b3975fdc4201048f32
gigaquantum <113395660+gigaquantum@users.noreply.github.com>
2024-01-24 21:36:06 -0500

    added importation from excel files

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,17 +1,26 @@
 <script setup>
 import { ref } from 'vue'
+import { read, utils } from 'xlsx'
 import { mainSort, rangeSort, algoFunctionOptions, arrayLen2D } from './sortingAlgo.js'
 import { testGroups, testTables } from './testData.js'
 
-const sortedTables = ref([])
+const sortedTables = ref(null)
 const minSeats = ref(null)
 const maxSeats = ref(null)
+const fileInput = ref(null)
 
-// console.log(rangeSort(testGroups, algoFunctionOptions, maxSeats.value, minSeats.value))
+async function getGroups() {
+  const uploadFile = await fileInput.value.files[0].arrayBuffer()
+  const workbook = read(uploadFile)
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+  const dataArray = utils.sheet_to_json(worksheet, { header: 1 })
+  return dataArray
+}
 
-function executeSort() {
+async function executeSort() {
+  const guestGroups = await getGroups()
   try {
-    sortedTables.value = rangeSort(testGroups, algoFunctionOptions, maxSeats.value, minSeats.value)
+    sortedTables.value = rangeSort(guestGroups, algoFunctionOptions, maxSeats.value, minSeats.value)
   } catch (error) {
     alert(error.message)
   }
@@ -20,19 +29,30 @@ function executeSort() {
 
 <template>
   <h1>Prom Table Sorting App</h1>
-  <label for="min-seats">Minimum Number of Seats per Table</label>
+  <label for="input-groups"
+    >Upload Group Excel File:
+    <br />
+    <span class="input-note"
+      >(Each row should have one person's name in each cell, each row is a seperate group.)</span
+    >
+  </label>
+  <br />
+  <input type="file" name="input-groups" ref="fileInput" accept=".xlsx" />
+  <br />
+  <button @click="getGroups">get groups</button>
+  <br />
+  <br />
+  <label for="min-seats">Minimum Number of Seats per Table: </label>
   <input v-model="minSeats" type="number" name="min-seats" min="0" step="1" />
   <br />
-  <label for="max-seats">Maximum Number of Seats per Table</label>
+  <label for="max-seats">Maximum Number of Seats per Table: </label>
   <input v-model="maxSeats" type="number" name="max-seats" min="1" step="1" />
   <br />
   <button @click="executeSort">Sort</button>
   <br />
   <br />
   <div class="table" v-for="table in sortedTables">
-    <p class="text">
-      {{ table.capacity - arrayLen2D(table.occupants) }}/{{ table.capacity }} Seats Unoccupied
-    </p>
+    <p class="text">{{ arrayLen2D(table.occupants) }}/{{ table.capacity }} Seats Occupied</p>
     <div class="group" v-for="group in table.occupants">
       <p class="person" v-for="person in group">{{ person }}</p>
     </div>
@@ -60,5 +80,9 @@ function executeSort() {
 
 .text {
   color: wheat;
+}
+
+.input-note {
+  font-style: italic;
 }
 </style>
