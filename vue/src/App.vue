46ff18c49b809ca7ef2e38cf617c499ab607a297
DariaG16 <dariagolitsyna16@gmail.com>
2024-04-04 10:58:58 -0400

    so i did a whole lot

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -184,180 +184,171 @@ async function exportResultsAsXLSX() {
 </script>
 
 <template>
-  <h1>Prom Table Sorting App</h1>
-  <br />
-  <div>
-    <label for="input-groups">Upload Group Excel File: </label>
+  <div id="app">
+    <div class="Ldiv">
+      <h1>Prom Sorter</h1>
+      <div class="form">
+        <div class="fileUpload">
+          <label for="input-groups"><h3>1. Upload group excel file:</h3></label>
+          <label class="uploadBtn btn" for="upload-file">Upload</label>
+          <input
+            id="upload-file"
+            class="btn"
+            type="file"
+            name="input-groups"
+            ref="fileInput"
+            accept=".xlsx"
+            required
+          />
+        </div>
+        <label class="" for="input-groups">Each row must be a separate group.</label>
+        <label for="data-format"><h3>2. Select data file format:</h3></label>
+        <div class="dataFormatContainer">
+          <input
+            class="checkbox"
+            v-model="dataFormat"
+            type="radio"
+            id="rows-columns"
+            value="rows-columns"
+            checked
+          />
 
-    <input type="file" name="input-groups" ref="fileInput" accept=".xlsx" required />
-    <br />
-    <br />
-    <label for="data-format">Input File Data Format:</label>
-    <br />
-    <input v-model="dataFormat" type="radio" id="rows-columns" value="rows-columns" checked />
-    <label for="rows-columns"
-      >Each row has one person's name in each cell, and each row is a seperate group.</label
-    >
-    <br />
-    <input
-      v-model="dataFormat"
-      type="radio"
-      id="single-cell-comma-seperated"
-      value="single-cell-comma-seperated"
-    />
-    <label for="single-cell-comma-seperated"
-      >One cell in each row has all of a group's names in it, with each name being seperated by a
-      comma and each row being a seperate group.
-      <span class="input-note">(Google Sheets)</span></label
-    >
-    <br />
-    <br />
-    <div v-if="searchAllCells" class="disabled">
-      <label for="cell-range">Cell Range: </label>
-      <input
-        disabled
-        v-model="cellRange[0]"
-        type="text"
-        size="4"
-        minlength="2"
-        name="cell-range"
-        id="cell-range"
-        required
-      />
-      :
-      <input
-        disabled
-        v-model="cellRange[1]"
-        type="text"
-        size="4"
-        minlength="2"
-        name="cell-range"
-        id="cell-range"
-        required
-      />
+          <label class="containerCheck" for="rows-columns"
+            >Each row has one person's name in each cell</label
+          >
+        </div>
+        <div class="dataFormatContainer">
+          <input
+            v-model="dataFormat"
+            class="checkbox"
+            type="radio"
+            id="single-cell-comma-seperated"
+            value="single-cell-comma-seperated"
+          />
+
+          <label class="containerCheck" for="single-cell-comma-seperated"
+            >One cell per row has all of a group's names in it
+            <span class="input-note">(Google Sheets)</span></label
+          >
+        </div>
+        <label for="cell-range"><h3>2. Select data file format:</h3></label>
+        <div>
+          <div v-if="searchAllCells" class="disabled rangeContainerFromto">
+            <input
+              disabled
+              v-model="cellRange[0]"
+              class="input-text cell-range"
+              type="text"
+              size="4"
+              minlength="2"
+              name="cell-range"
+              required
+            />
+            :
+            <input
+              disabled
+              v-model="cellRange[1]"
+              class="input-text cell-range"
+              type="text"
+              size="4"
+              minlength="2"
+              name="cell-range"
+              required
+            />
+          </div>
+          <div class="rangeContainerFromto" v-else>
+            <label for="cell-range">From: </label>
+            <input
+              v-model="cellRange[0]"
+              class="input-text cell-range"
+              type="text"
+              size="4"
+              minlength="2"
+              name="cell-range"
+              required
+              placeholder="A1"
+            />
+            <label for="cell-range">To: </label>
+            <input
+              v-model="cellRange[1]"
+              class="input-text cell-range"
+              type="text"
+              size="4"
+              minlength="2"
+              name="cell-range"
+              required
+              placeholder="B2"
+            />
+          </div>
+          <div class="searchAllCells">
+            <input
+              type="checkbox"
+              class="checkbox"
+              name="search-all-cells"
+              id="search-all-cells"
+              v-model="searchAllCells"
+              minlength="5"
+            />
+            <label for="search-all-cells">Search All Cells</label>
+          </div>
+        </div>
+        <label for="seat-range"><h3>4. Define range of seats per table</h3></label>
+        <div name="seat-range" class="rangeContainerMinmax">
+          <label for="min-seats">Minimum:</label>
+          <input
+            v-model="minSeats"
+            class="input-text"
+            type="number"
+            name="min-seats"
+            min="0"
+            step="1"
+            required
+          />
+          <label for="max-seats">Maximum:</label>
+          <input
+            v-model="maxSeats"
+            type="number"
+            class="input-text"
+            name="max-seats"
+            min="1"
+            step="1"
+            required
+          />
+        </div>
+        <button @click="executeSort" class="btn" id="sortBtn">Sort</button>
+      </div>
     </div>
-    <div v-else>
-      <label for="cell-range">Cell Range: </label>
-      <input
-        v-model="cellRange[0]"
-        type="text"
-        size="4"
-        minlength="2"
-        name="cell-range"
-        id="cell-range"
-        required
-        placeholder="A1"
-      />
-      :
-      <input
-        v-model="cellRange[1]"
-        type="text"
-        size="4"
-        minlength="2"
-        name="cell-range"
-        id="cell-range"
-        required
-        placeholder="B2"
-      />
-    </div>
-    <input
-      type="checkbox"
-      name="search-all-cells"
-      id="search-all-cells"
-      v-model="searchAllCells"
-      minlength="5"
-    />
-    <label for="search-all-cells">Search All Cells</label>
-    <br />
-    <br />
-    <label for="min-seats">Minimum Number of Seats per Table: </label>
-    <input v-model="minSeats" type="number" name="min-seats" min="0" step="1" required />
-    <br />
-    <label for="max-seats">Maximum Number of Seats per Table: </label>
-    <input v-model="maxSeats" type="number" name="max-seats" min="1" step="1" required />
-    <br />
-    <button @click="executeSort">Sort</button>
-  </div>
 
-  <br />
-  <br />
-  <div v-if="sortedTables != null">
-    <p>----------</p>
-    <a v-if="downloadURL == null" disabled class="btn-link">Loading...</a>
-    <a v-else :href="downloadURL" class="btn-link">Download Sorted Tables</a>
-  </div>
-
-  <br />
-  <div class="table" v-for="table in sortedTables">
-    <p class="text">{{ arrayLen2D(table.occupants) }}/{{ table.capacity }} Seats Occupied</p>
-    <div class="group" v-for="group in table.occupants">
-      <div v-for="person in group">
-        <p v-if="person.name == 'guest name'" class="person unnamed">
-          <mark>{{ person.name }}</mark>
-        </p>
-        <p v-else-if="person.duplicate == true" class="person duplicate">
-          <mark>{{ person.name }}</mark>
-        </p>
-        <p v-else class="person">{{ person.name }}</p>
+    <div class="Rdiv">
+      <div id="navBar">
+        <h2>Generated Tables</h2>
+        <div v-if="sortedTables != null">
+          <a v-if="downloadURL == null" disabled class="btn">Loading...</a>
+          <a v-else :href="downloadURL" class="btn">Download Sorted Tables</a>
+        </div>
+      </div>
+      <div id="tables">
+        <div class="table" v-for="table in sortedTables">
+          <p class="text">{{ arrayLen2D(table.occupants) }}/{{ table.capacity }} Seats Occupied</p>
+          <div class="groupsInTable">
+            <div class="group" v-for="group in table.occupants">
+              <div v-for="person in group">
+                <p v-if="person.name == 'guest name'" class="person unnamed">
+                  <mark>{{ person.name }}</mark>
+                </p>
+                <p v-else-if="person.duplicate == true" class="person duplicate">
+                  <mark>{{ person.name }}</mark>
+                </p>
+                <p v-else class="person">{{ person.name }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.table {
-  background-color: brown;
-  padding: 0.5rem;
-  margin: 0.5rem;
-}
-
-.group {
-  background-color: bisque;
-  padding: 0.5rem;
-  margin: 0.5rem;
-}
-
-.person {
-  background-color: cadetblue;
-  padding: 0.5rem;
-  margin: 0.5rem;
-}
-
-.text {
-  color: wheat;
-}
-
-.input-note {
-  font-style: italic;
-}
-
-.btn-link {
-  font: 12.5px Arial;
-  text-decoration: none;
-  background-color: #e9e9ed;
-  color: #000000;
-  padding: 5px;
-  border: 1px solid #8f8f9d;
-  border-radius: 4.5px;
-}
-.btn-link:hover {
-  background-color: #d0d0d7;
-  border-color: #676774;
-}
-
-.btn-link:active {
-  background-color: #b1b1b9;
-  border-color: #484851;
-}
-
-.duplicate mark {
-  background-color: #f5ca50;
-}
-
-.unnamed mark {
-  background-color: #f55050;
-}
-.disabled {
-  opacity: 60%;
-}
+@import url(./assets/main.css);
 </style>
