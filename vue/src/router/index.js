5dd59be4115e6ddc85abb187c6e32ee53e290c09
gigaquantum <113395660+gigaquantum@users.noreply.github.com>
2024-05-02 11:00:02 -0400

    started adding additional views & routing

---
+++ vue/src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/name/:spreadsheetArray',
+      name: 'Upload Data',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/UploadData.vue'),
+      props: true
+    },
+    {
+      path: '/confirmation/:orderedItem/:customerName',
+      name: 'Order Received',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/OrderReceived.vue'),
+      props: true
+    }
+  ]
+})
+
+export default router
